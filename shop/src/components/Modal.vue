572239b21c7390dd2008e724d520d46e734d7787
““hu5210“ <280383929@qq.com>
2019-07-03 12:35:53 +0800

    vue的代码

---
+++ shop/src/components/Modal.vue
@@ -0,0 +1,51 @@
+<template>
+ <div class="modal">
+   <div class="Modal">
+      <div class="md-modal modal-msg md-modal-transition" v-bind:class="{'md-show':isMdShow}">
+        <div class="md-modal-inner">
+          <!-- 弹框关闭 -->
+          <div class="md-top">
+          <slot name="title"></slot>
+            <button class="md-close" @click="modelshow">Close</button>
+          </div>
+          <!-- 弹框内数据 -->
+          <div class="md-content">
+            <div class="confirm-tips">
+              <slot name="main"></slot>
+            </div>
+            <div class="btn-wrap">
+               <slot name="btn"></slot>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- 
+      灰色遮罩
+      知识点： v-if 控制DOM（HTML代码不显示）  v-show 控制样式（通过display不显示）
+      -->
+      <div class="md-overlay" v-if="isMdShow" @click="modelshow"></div>
+    </div>
+ </div>
+</template>
+ 
+<script>
+export default {
+  props:['isMdShow'],
+  data(){
+    return {
+      //是否显示遮罩:true-是，false-否
+      // isMdShow: true
+    }
+  },
+  methods:{
+   modelshow(){
+     this.$emit("close")//创建自定义事件
+   }
+
+  }
+ }
+</script>
+ 
+<style scoped>
+ 
+</style>
---
+++ shop/src/components/NavBread.vue
@@ -0,0 +1,13 @@
+<template>
+<div class="NavBread">
+    <nav class="nav-breadcrumb">
+        <slot name="content"></slot> 
+    </nav>
+</div> 
+</template>
+<script>
+export default {
+}
+</script>
+<style scoped>
+</style>
